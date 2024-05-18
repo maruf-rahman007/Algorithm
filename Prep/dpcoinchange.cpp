402140e95fd328e6d5fbc04ab57c4b259d0be204
maruf-rahman007 <s.marufrahman.g@gmail.com>
2024-05-18 21:06:59 +0600

    preparational ps

---
+++ Prep/dpcoinchange.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int coins[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>coins[i];
+    }
+    int target;
+
+    cin>>target;
+    int dp[target+1];
+    memset(dp,0,sizeof(dp));
+    dp[0]=1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = coins[i]; j <= target; j++)
+        {
+            dp[j]+=dp[j - coins[i]];
+        }
+        
+    }
+    cout<<dp[target]<<endl;
+    
+    return 0;
+}
